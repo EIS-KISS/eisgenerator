a219f9095f05b71b94488214171cce1890712a79
Carl Philipp Klemm <philipp@uvos.xyz>
2024-01-08 14:50:46 +0100

    add license headers

--- compile.h
+++ compile.h
@@ -1,3 +1,22 @@
+/* * eisgenerator - a shared libary and application to generate EIS spectra
+ * Copyright (C) 2022-2024 Carl Philipp Klemm <carl@uvos.xyz>
+ *
+ * This file is part of eisgenerator.
+ *
+ * eisgenerator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * eisgenerator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with eisgenerator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <string>
 
 namespace eis
